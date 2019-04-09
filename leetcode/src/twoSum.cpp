124c0cc5649bdd9017d1d99eabd15c6dc227f332
zhonglongbo <17629272227@163.com>
2019-04-09 17:30:45 +0800

    update

--- leetcode/src/twoSum.cpp
+++ leetcode/src/twoSum.cpp
@@ -1,12 +1,13 @@
 /*
- * Created by  2019/4/9 12:37
- * Author: zhonglongbo
- * Email:17629272227@163.com
+ * Created by 2019/4/9
+ * Author: big shot
+ * Email: 17629272227@163.com
  */
 
 #include <map>
 #include <iostream>
 #include <stdio.h>
+#include <vector>
 
 /*
  * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，
@@ -29,17 +30,17 @@ int Find(std::vector<int> &nums, int first, int end, int target) {
  * 直接在数组的剩下元素中查找target-nums[i]，是否存在
  * 时间复杂度为O(N)，空间复杂度为O(1)
  */
-std::vector<int> twoSum(std::vector<int> &nums, int target) {
-    std::vector<int> ret;
-    for (int i = 0, size = nums.size(); i < size; ++i) {
-        int pos = Find(nums, i + 1, size, target - nums[i]);
-        if (pos != -1) {
-            ret.push_back(i);
-            ret.push_back(pos);
-        }
-    }
-    return ret;
-}
+//std::vector<int> twoSum(std::vector<int> &nums, int target) {
+//    std::vector<int> ret;
+//    for (int i = 0, size = nums.size(); i < size; ++i) {
+//        int pos = Find(nums, i + 1, size, target - nums[i]);
+//        if (pos != -1) {
+//            ret.push_back(i);
+//            ret.push_back(pos);
+//        }
+//    }
+//    return ret;
+//}
 
 /*
  * 用哈希表的方式查找
@@ -51,12 +52,12 @@ std::vector<int> twoSum(std::vector<int> &nums, int target) {
     std::vector<int> answer;
     int len = nums.size();
     for (int i = 0; i < len; ++i) {
-        mp[nums[i]] = i;
+        mp.insert(std::pair<int,int>(nums[i],i));
         auto ret = mp.find(target - nums[i]);
         if (ret != mp.end() && ret->second != -1 && ret->second != i) {
-            answer.push_back(i);
             answer.push_back(ret->second);
-            mp[nums[i]] = -1;
+            answer.push_back(i);
+            mp.find(nums[i])->second = -1;
         }
     }
     return answer;
@@ -65,33 +66,34 @@ std::vector<int> twoSum(std::vector<int> &nums, int target) {
 /*
  * 用两边哈希，效果与上面一样
  */
-std::vector<int> twoSum(std::vector<int> &nums, int target) {
-    std::multimap<int, int> mp;
-    std::vector<int> answer;
-    int len = nums.size();
-    for (int i = 0; i < len; ++i) {
-        mp[nums[i]] = i;
-    }
-    for (int j = 0; j < len; ++j) {
-        auto ret = mp.find(target - nums[j]);
-        if (ret != mp.end() && ret->second != -1 && ret->second != j) {
-            answer.push_back(j);
-            answer.push_back(ret->second);
-            mp[nums[j]] = -1;
-        }
-    }
-    return answer;
-}
+//std::vector<int> twoSum(std::vector<int> &nums, int target) {
+//    std::multimap<int, int> mp;
+//    std::vector<int> answer;
+//
+//    int len = nums.size();
+//    for (int i = 0; i < len; ++i) {
+//        mp.insert(std::pair<int, int>(nums[i], i));
+//    }
+//    for (int j = 0; j < len; ++j) {
+//        auto ret = mp.find(target - nums[j]);
+//        if (ret != mp.end() && ret->second != -1 && ret->second != j) {
+//            answer.push_back(j);
+//            answer.push_back(ret->second);
+//            mp.find(nums[j])->second = -1;
+//        }
+//    }
+//    return answer;
+//}
 
 void test_twoSum() {
-    std::vector<int> array = {3,3};
+    std::vector<int> array = {2,7,11,15};
     int target = 9;
     std::vector<int> ret;
     ret = twoSum(array, target);
     for (int i = 0; i < ret.size(); ++i) {
-        std::cout<<ret[i];
+        std::cout << ret[i];
     }
-    std::cout<<std::endl;
+    std::cout << std::endl;
 }
 
 
@@ -99,3 +101,4 @@ int main() {
     test_twoSum();
     return 0;
 }
+
